dc68bdaebb45a8b884466fce54a853341fb9e84c
Shivesh Kumar Tripathi <20072713+shiveshtripathi@users.noreply.github.com>
2024-08-21 15:33:42 +0530

    Complete01_helloWorld and 02_repeatString problem

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function (greeting = `hey`, times = 3) {
+  if (times < 0) return 'ERROR'
+  let greetingMsg = ''
+  for (i = 0; i < times; i++) {
+    greetingMsg = greetingMsg + greeting
+  }
+  return greetingMsg
+}
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString
