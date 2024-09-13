a893dffbed5cfd6242341cf268f6d96ee8df1db2
Shivesh Kumar Tripathi <20072713+shiveshtripathi@users.noreply.github.com>
2024-09-13 14:47:04 +0530

    Complete the 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (originalArray, ...args) {
+  for (let arg of args) {
+    let indexOfElement = originalArray.indexOf(arg)
+    let lastIndexOfElement = originalArray.lastIndexOf(arg)
+    if (indexOfElement === -1) {
+      continue
+    } else if (indexOfElement !== lastIndexOfElement) {
+      while (indexOfElement !== lastIndexOfElement) {
+        originalArray.splice(indexOfElement, 1)
+        if (originalArray.includes(arg)) {
+          indexOfElement = originalArray.indexOf(arg)
+          continue
+        }
+        indexOfElement = originalArray.indexOf(arg)
+        lastIndexOfElement = originalArray.lastIndexOf(arg)
+      }
+    } else if (indexOfElement === lastIndexOfElement) {
+      originalArray.splice(indexOfElement, 1)
+    }
+  }
+  return originalArray
+}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
